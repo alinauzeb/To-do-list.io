95e519365679d20d8085b8ab311f5d90519a1d09
Ali Nauzeb <Anauzeb@gmail.com>
2022-02-27 11:56:29 +0530

    My To-Do list

---
+++ main.js
@@ -0,0 +1,11 @@
+let xyz;
+
+const newLocal = "header";
+document.getElementById("header").addEventListener("click", abc);
+
+
+function abc() {
+
+  document.getElementById("header").style.background = "green";
+
+}
